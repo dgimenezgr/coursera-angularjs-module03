9e02a2bafbe2d7e98a1b48f8bdb3ebd1b3c8c341
Daniel Giménez <danielgimenezgredilla@hotmail.com>
2020-06-16 15:01:05 +0200

    Filtering functionality refactoring.

--- app.js
+++ app.js
@@ -37,12 +37,15 @@ function NarrowItDownController(MenuSearchFactory) {
 
   narrowItDownList.getMatchedItems = function () {
 
+  // narrowItDownList.found = menuSearchList.getMatchedMenuItems(narrowItDownList.term);
+
+  // console.log(narrowItDownList.found);
+
+
       var promise = menuSearchList.getMatchedMenuItems(narrowItDownList.term);
 
       promise.then(function (response) {
-        narrowItDownList.found = response.data;
-
-        narrowItDownList.found = menuSearchList.filterMenuItems(narrowItDownList.found, narrowItDownList.term);
+        narrowItDownList.found = response;
       })
       .catch(function (){
         console.log('Not working');
@@ -60,16 +63,19 @@ function MenuSearchService($http,ApiEndpoint) {
   var service = this;
   
   service.getMatchedMenuItems = function (term) {
-    var response = $http({
+    return $http({
       method: "GET",
       url: (ApiEndpoint + "/menu_items.json")
-    });
+    }).then(function(result) {
+      var foundItems = result.data.menu_items.filter( function(e) {
+        return e.description.toLowerCase().includes(term.toLowerCase());
+      });
 
-    return response;
+      return foundItems;
+    });
   }    
 
   service.filterMenuItems = function (response, term) {
-    console.log("TERM 1: " + term);
     var found = response.menu_items.filter( function(e) {
       return e.description.toLowerCase().includes(term.toLowerCase());
     });
